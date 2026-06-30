9fed49af793bec8463da977730e8ead29af5e9b5
sameer-o18 <shaikh18sameer018@gmail.com>
2026-06-30 18:33:43 +0530

    Two sum solution with O(n^2) time complexity

---
+++ problem0001.cpp
@@ -0,0 +1,29 @@
+// this is first problem of leetcode TWO SUM
+//Problem:
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//this solution will have O(n^2) time complexity
+class Solution{
+    public:
+        vector<int> twosum(vector<int> &nums, int target){
+            for(int i=0; i<nums.size(); i++){
+                for(int j=0; j<nums.size(); j++){
+                    if(nums[i]+nums[j]==target && i!=j){
+                        return {i,j};
+                    }
+                }
+            }
+            return {0,1};
+        }
+};
+
+int main(){
+
+    return 0;
+}

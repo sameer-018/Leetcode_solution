f4de927d8f738fa2154b4edad24a8c0c23e89acf
Sameer <shaikh18sameer018@gmail.com>
2026-07-23 20:25:40 +0530

    Finding Middle of Linked List

---
+++ p0876.cpp
@@ -0,0 +1,42 @@
+// Finding Middle of Linked List
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *mover = head;
+        int count = 0;
+        while (mover)
+        {
+            count++;
+            mover = mover->next;
+        }
+        int mid;
+        mid = count / 2 + 1;
+        count = 0;
+        mover = head;
+        while (count != mid - 1)
+        {
+            count++;
+            mover = mover->next;
+        }
+        return mover;
+    }
+};

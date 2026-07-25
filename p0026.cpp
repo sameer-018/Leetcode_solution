d6a7f6ed222d3f356df5b82e2c21efae272d6c54
Sameer <shaikh18sameer018@gmail.com>
2026-07-25 22:37:19 +0530

    Remove Duplicate from Sorted Array

---
+++ p0026.cpp
@@ -0,0 +1,23 @@
+//Remove Duplicate From sorted array
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+// Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+// The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+# include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int slow=0;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] != nums[slow]){
+                slow++;
+                nums[slow]=nums[i];
+            }
+        }
+        return slow+1;
+    }
+};
